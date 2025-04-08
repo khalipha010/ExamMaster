f3866132d2bcbba5066a4a69efacefe6ab20ef6f
Khalifa Jibreel <khalifajibreel@gmail.com>
2025-04-08 22:24:59 +0100

    vite update

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,8 @@ export default defineConfig({
     port: 5173,
     strictPort: true,
   },
+  build: {
+    outDir: "dist", // Ensure output directory is 'dist'
+    assetsDir: "assets", // Where static assets are stored
+  },
 });
