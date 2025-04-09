7d5a4398e3e149b30cbfbaeb22969ad6afb4c173
Khalifa_Jibreel <57154132+khalipha010@users.noreply.github.com>
2025-04-09 01:30:24 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    strictPort: true,
-  },
 });
